96e394414c64dec2e09c08d0d417900e1cf8951b
Brandon <brandon@mogul-tech.com>
2024-07-25 00:15:11 +0800

    update

--- functions/api/upload.js
+++ functions/api/upload.js
@@ -1,10 +1,18 @@
 export async function onRequestPost(context) {
+    const {
+        request, // same as existing Worker API
+        env, // same as existing Worker API
+        params, // if filename includes [id] or [[path]]
+        waitUntil, // same as ctx.waitUntil in existing Worker API
+        next, // used for middleware or to fetch assets
+        data, // arbitrary space for passing data between middlewares
+    } = context;
 
     // 相册
-    const formData = await context.request.formData()
+    const formData = await request.formData()
     const albumName = formData.get('x-album-name')
 
-    const req = context.request.clone()
+    const req = request.clone()
     const response = await fetch('https://telegra.ph/upload', {
         method: req.method,
         headers: req.headers,
@@ -14,8 +22,8 @@ export async function onRequestPost(context) {
     const result = await response.json()
 
     if (result && result[0] && result[0].src && albumName) {
-        await context.env.telegraph_image_album.put(albumName, '')
-        await context.env.telegraph_image_url.put(albumName + '_' + result[0].src, '')
+        await env.telegraph_image_album.put(albumName, '')
+        await env.telegraph_image_url.put(albumName + '_' + result[0].src, '')
     }
 
     return new Response(JSON.stringify(result), {
