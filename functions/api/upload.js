abd219dc9c7ffc3a31d0370a6f23a83e153cc84b
Brandon <39255573+rombrandon@users.noreply.github.com>
2024-07-24 15:25:40 +0800

    Create upload.js

---
+++ functions/api/upload.js
@@ -0,0 +1,21 @@
+export async function onRequestPost(context) {  // Contents of context object
+    const {
+        request, // same as existing Worker API
+        env, // same as existing Worker API
+        params, // if filename includes [id] or [[path]]
+        waitUntil, // same as ctx.waitUntil in existing Worker API
+        next, // used for middleware or to fetch assets
+        data, // arbitrary space for passing data between middlewares
+    } = context;
+    const clonedRequest = request.clone()
+    const url = new URL(clonedRequest.url)
+    const response = await fetch('https://telegra.ph/upload', {
+        method: clonedRequest.method,
+        headers: clonedRequest.headers,
+        body: clonedRequest.body,
+    });
+
+    response.headers.set('x-log', JSON.stringify(params))
+
+    return response
+}
